96bb856d80f2d1a396e44028e24d27bd71662d91
Unknown <jun.quan.mail@gmail.com>
2018-09-05 14:09:50 +0700

    styling the chat app

--- public/js/index.js
+++ public/js/index.js
@@ -19,6 +19,17 @@ socket.on('newMessage', function (message) {
   jQuery('#messages').append(li);
 })
 
+socket.on('newLocation', function (location) {
+  console.log('Location ', location);
+  var li = jQuery('<li></li>');
+  var a = jQuery('<a target="_blank">My current location</a>');
+  li.text(`${location.from}: `);
+  a.attr('href', location.url);
+  li.append(a);
+
+  jQuery('#messages').append(li);
+})
+
 socket.on('disconnect', function () {
   console.log('Disconnected from server');
 })
@@ -26,10 +37,23 @@ socket.on('disconnect', function () {
 jQuery('#message-form').on('submit', function (e) {
   e.preventDefault();
 
+  let messageTextbox = jQuery('[name=message]');
+
   socket.emit('createMessage', {
     from: 'User',
     text: jQuery('[name=message]').val()
   }, function () {
+    messageTextbox.val('');
+  })
+})
+
+var locationButton = jQuery('#send-location');
+locationButton.on('click', function() {
+  if (!navigator.geolocation) {
+    return alert('Geolocation is not supported by your Browser')
+  }
+
+  locationButton.attr('disabled', 'disabled').text('Sending location...');
 
   navigator.geolocation.getCurrentPosition(function(position) {
     locationButton.removeAttr('disabled').text('Send Location');
